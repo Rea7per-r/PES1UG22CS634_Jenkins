c720a21a935fc01c003444d0da99971962acaf38
Suraj-R <118007761+Rea7per-r@users.noreply.github.com>
2025-03-13 16:37:46 +0530

    Updateee hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "suraj \n";
+  cout << "suraj123 \n";
   return 0;
 }
